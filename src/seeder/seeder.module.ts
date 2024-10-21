e1acd1a75d6b1f444c1df2a5f05f2dabd69bfa0f
aqeel-saeed <aqeelraiee210@gmail.com>
2024-10-21 15:52:50 +0300

    fix seeder

--- src/seeder/seeder.module.ts
+++ src/seeder/seeder.module.ts
@@ -4,12 +4,22 @@ import { PermissionSeeder } from './seeders/permission.seeder';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Permission } from '../core/modules/permissions/entities/permission.entity';
 import { DatabaseModule } from '../core/configurations/database/database.module';
+import { Role } from '../core/modules/roles/entities/role.entity';
+import { User } from '../core/modules/users/entities/user.entity';
+import { Address } from '../core/modules/users/entities/address.entity';
+import { Post } from '../core/modules/posts/entities/post.entity';
+import { Category } from '../core/modules/categories/entities/category.entity';
 
 @Module({
   imports: [
     DatabaseModule,
     TypeOrmModule.forFeature([
-      Permission
+      Permission,
+      Role,
+      User,
+      Address,
+      Post,
+      Category
     ])
   ],
   providers: [
