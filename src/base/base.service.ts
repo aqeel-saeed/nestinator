369d9f8502b30bd161321500e0538a4711bfa17b
aqeel-saeed <aqeelraiee210@gmail.com>
2024-10-23 16:20:59 +0300

    add base service

---
+++ src/base/base.service.ts
@@ -0,0 +1,102 @@
+import { DeepPartial, In, Repository } from "typeorm";
+import { BaseRepository } from "./base.repository";
+import { NotFoundException } from "@nestjs/common";
+
+export class BaseService<T> {
+    constructor(
+        protected readonly repository: BaseRepository<T>,
+    ) {}
+
+    private async resolveRelations(data: DeepPartial<T> | DeepPartial<T>[]): Promise<any | any[]> {
+        const isArray = Array.isArray(data);
+        // warpping single data into an array for processing
+        const items = isArray ? data : [data]; 
+        const resolvedItems = await Promise.all(items.map(async (item) => {
+            const resolvedItem = { ...item };
+            const entityMetadata = this.repository.metadata;
+            const relations = entityMetadata.relations;
+            
+            for (const relation of relations) {
+            // Check whether the relation is singular (ManyToOne, OneToOne) or plural (OneToMany, ManyToMany)
+            const isSingularRelation = relation.isManyToOne || relation.isOneToOne;
+            const isPluralRelation = relation.isManyToMany || relation.isOneToMany;
+
+            // Assign singular or plural based on the relationship type
+            const relationId = isSingularRelation
+                ? `${relation.propertyName}Id`
+                : `${relation.propertyName}Ids`;
+
+                if (isSingularRelation) {
+                    if (resolvedItem[relationId]) {
+                        const relatedEntity = await this.repository.manager.getRepository(relation.type).findOne({
+                            where: {id: resolvedItem[relationId] },
+                        });
+                        if (!relatedEntity) {
+                            throw new NotFoundException(
+                                `${relation.propertyName} not found.`
+                            );
+                        }
+                        resolvedItem[relation.propertyName] = relatedEntity;
+                    }
+                } else if (isPluralRelation) {
+                    if (Array.isArray(resolvedItem[relationId])) {
+                        const relatedEntities = await this.repository.manager.getRepository(relation.type).findBy({
+                            id: In(resolvedItem[relationId]),
+                        });
+                        if (relatedEntities.length !== resolvedItem[relationId].length) {
+                            throw new NotFoundException(
+                                `${relation.propertyName} not all entities found.`
+                            );
+                        }
+                        resolvedItem[relation.propertyName] = relatedEntities;
+                    }
+                }
+            }
+
+            return resolvedItem;
+        }));
+
+        // return array of resolved items or a single item
+        return isArray ? resolvedItems: resolvedItems[0]; 
+    }
+
+    async findAll(): Promise<T[]> {
+        return await this.repository.find();
+    }
+
+    async findById(id: number): Promise<T> {
+        const entity = await this.repository.findOne({
+            where: { id } as any,
+        });
+        if (!entity) {
+            // TODO: replace this with our custom exception
+            throw new NotFoundException(
+                `Entity with ID ${id} not found`
+            );
+        }
+        return entity;
+    }
+
+    async create(data: DeepPartial<T> | DeepPartial<T>[]): Promise<T | T[]> {
+        const resolvedData = await this.resolveRelations(data);
+    
+        // Debugging
+        console.log('resolvedData is:', resolvedData);
+
+        const entities = this.repository.create(resolvedData);
+        return await this.repository.save(entities);
+    }
+
+    async update(id: number, data: DeepPartial<T>): Promise<T> {
+        const entity = await this.findById(id);
+        const resolvedData = await this.resolveRelations(data);
+        Object.assign(entity, resolvedData);
+        return await this.repository.save(entity);
+    }
+
+    async delete(id: number): Promise<void> {
+        // call the findById for the id to check if entity with id is exited or not
+        await this.findById(id);
+        await this.repository.delete(id);
+    }
+}
