63e15487950abfbb19c317eafad66137f8f40215
aqeel-saeed <aqeelraiee210@gmail.com>
2024-10-28 16:38:05 +0300

    add the conditional decorator

---
+++ src/shared/decorators/conditional.decorator.ts
@@ -0,0 +1,11 @@
+export function ConditionalDecorator(condition: boolean, decorator: MethodDecorator) {
+    // if condition is true, apply the decorator
+    if (condition) {
+        return decorator;
+    }
+
+    // if condition is false, return no-op decorator
+    return () => {
+        // Do nothing
+    };
+}
