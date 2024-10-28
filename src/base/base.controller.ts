ff05b9d2640f5ae50d27387a77a2a1e5c306c390
aqeel-saeed <aqeelraiee210@gmail.com>
2024-10-28 16:25:38 +0300

    delete un used decorators in base controller

--- src/base/base.controller.ts
+++ src/base/base.controller.ts
@@ -1,12 +1,9 @@
 import { Body, Delete, Get, Param, Post, Put, Req, UseGuards } from "@nestjs/common";
 import { BaseService } from "./base.service";
-import { ApiBearerAuth, ApiParam } from "@nestjs/swagger";
+import { ApiParam } from "@nestjs/swagger";
 import { FindOneParams } from "src/shared/params/find-one.params";
-import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 import { DeepPartial } from "typeorm";
 import { apiResponse } from "src/core/utils/utils";
-import { EnumType } from "typescript";
-import { RequirePermissions } from "src/core/modules/permissions/decorators/permissions.decorator";
 
 export class BaseController<
     T,
@@ -30,16 +27,12 @@ export class BaseController<
         return apiResponse(res, 'Item retrieved successfully.');
     }
 
-    @ApiBearerAuth('Authorization')
-    @UseGuards(JwtAuthGuard)
     @Post()
     async create(@Body() data: CreateDto, @Req() req) {
         const res = await this.service.create(data);
         return apiResponse(res, 'Item created successfully.')
     }
 
-    @ApiBearerAuth('Authorization')
-    @UseGuards(JwtAuthGuard)
     @ApiParam({ name: 'id', type: Number, description: 'ID of the item' })
     @Put(':id')
     async update(@Param() { id }: FindOneParams, @Body() data: UpdateDto, @Req() req) {
@@ -47,8 +40,6 @@ export class BaseController<
         return apiResponse(res, 'Item updated successfully.');
     }
 
-    @ApiBearerAuth('Authorization')
-    @UseGuards(JwtAuthGuard)
     @ApiParam({ name: 'id', type: Number, description: 'ID of the item' })
     @Delete(':id')
     async delete(@Param() { id }: FindOneParams) {
