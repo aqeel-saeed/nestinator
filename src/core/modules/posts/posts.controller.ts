16b643f0bd9a7fca68c3fc076f8e2d6d956abb11
aqeel-saeed <aqeelraiee210@gmail.com>
2024-10-28 16:26:28 +0300

    test using the controller permissions decorator in post controller

--- src/core/modules/posts/posts.controller.ts
+++ src/core/modules/posts/posts.controller.ts
@@ -1,25 +1,30 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PostsService } from './posts.service';
-import { ApiBearerAuth, ApiParam, ApiTags } from '@nestjs/swagger';
-import { FindOneParams } from 'src/shared/params/find-one.params';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { apiResponse } from 'src/core/utils/utils';
 import { BaseController } from 'src/base/base.controller';
 import { Post as PostEntity } from './entities/post.entity';
+import { ControllerPermissions } from '../permissions/decorators/controller-permissions.decorator';
+import { PostPermissionsEnum } from './permissions/posts-permissions.enum';
+import { PermissionsGuard } from '../permissions/guards/permissions.guard';
  
 @ApiTags('Posts')
 @Controller('posts')
+@ControllerPermissions({
+  'create': [PostPermissionsEnum.POST_CREATE],
+})
 export class PostsController extends BaseController<PostEntity, CreatePostDto, UpdatePostDto> {
   constructor(
-    private readonly postsService: PostsService
+    private readonly postsService: PostsService,
   ) {
     super(postsService);
   }
   
   @ApiBearerAuth('Authorization')
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
   @Post()
   async create(@Body() post: CreatePostDto, @Req() req) {
     post['authorId'] = req.user.userId;
